3cd83e78be1c10aa8e567b3254dc23d78aaa8254
Sabeer Sulaiman <sabeer@fantacode.com>
2017-10-21 11:32:00 +0530

    Demo Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { DataTableModule } from 'ngx-simple-datatable';
+import { DataTableDemo1 } from './demo1/data-table-demo1';
+import { DataTableDemo2 } from './demo2/data-table-demo2';
+import { DataTableDemo3 } from './demo3/data-table-demo3';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableDemo1,
+    DataTableDemo2,
+    DataTableDemo3
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DataTableModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
